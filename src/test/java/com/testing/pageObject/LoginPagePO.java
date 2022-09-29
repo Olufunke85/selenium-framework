7cdf9e9bead4b2cda88d7740d4927b82b265f8e8
Omolola <eventthrillers@gmail.com>
2022-09-29 22:06:00 +0100

    new codes added

---
+++ src/test/java/com/testing/pageObject/LoginPagePO.java
@@ -0,0 +1,26 @@
+package com.testing.pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class LoginPagePO {
+    //Stage 1- Element Locators
+@FindBy(id ="Email")
+    public static WebElement EmailField;
+@FindBy(id ="Password" )
+    public static WebElement PasswordField;
+
+
+    
+
+
+
+
+
+
+
+
+}
